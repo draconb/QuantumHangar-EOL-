4a641b914e36b04671a8a6a5be23c48491d63109
Lilith <lilithcyber@tutanota.com>
2024-11-26 00:09:15 +0000

    Admin commands for faction hangar (#1)
    * Added in old admin faction hangar commands.
    
    * General code cleanup.
    
    * Added checks to gracefully handle exception when the targeted faction is founded by a NPC.
    
    * Fixed borked crash fix for the LoadGrid method.

---
+++ Quantumhangar/Commands/Faction/FactionHangarAdminCommands.cs
@@ -0,0 +1,225 @@
+﻿using QuantumHangar.HangarChecks;
+using Torch.Commands;
+using Torch.Commands.Permissions;
+using VRage.Game.ModAPI;
+
+namespace QuantumHangar.Commands;
+
+/// <summary>
+///     Command module for faction hangar admin commands.
+/// </summary>
+[Category("factionhangermod")]
+public class FactionHangarAdminCommands : CommandModule
+{
+    /// <summary>
+    ///     Adds or removes a player from the whitelist.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="targetNameOrSteamId">The target player's name or Steam ID.</param>
+    [Command("whitelist", "Add or remove a player from whitelist")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Whitelist(string tag, string targetNameOrSteamId)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.ChangeWhitelist(targetNameOrSteamId));
+    }
+
+    /// <summary>
+    ///     Outputs the whitelisted Steam IDs.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    [Command("whitelisted", "Output the whitelisted steam ids")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Whitelist(string tag)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.ListWhitelist());
+    }
+
+    /// <summary>
+    ///     Changes the webhook.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="webhook">The new webhook URL.</param>
+    [Command("webhook", "Change the webhook")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Webhook(string tag, string webhook)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.ChangeWebhook(webhook));
+    }
+
+    /// <summary>
+    ///     Saves the grid the player is looking at to the hangar.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="gridName">The name of the grid.</param>
+    [Command("save", "Saves the grid you are looking at to hangar")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void SaveGrid(string tag, string gridName)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.SaveGrid(gridName));
+    }
+
+    /// <summary>
+    ///     Lists all the grids saved in the hangar.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    [Command("list", "Lists all the grids saved in your hangar")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void ListGrids(string tag)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.ListGrids());
+    }
+
+    /// <summary>
+    ///     Loads the specified grid by index number.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="id">The grid ID.</param>
+    /// <param name="loadNearPlayer">Whether to load the grid near the player.</param>
+    [Command("load", "Loads the specified grid by index number")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Load(string tag, string id, bool loadNearPlayer = false)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.LoadGrid(id, loadNearPlayer));
+    }
+
+    /// <summary>
+    ///     Removes the grid from the hangar.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="id">The grid ID.</param>
+    [Command("remove", "removes the grid from your hangar")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Remove(string tag, string id)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.RemoveGrid(id));
+    }
+
+    /// <summary>
+    ///     Provides information about the current grid in the hangar.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="id">The grid ID (optional).</param>
+    [Command("info", "Provides some info of the current grid in your hangar")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Info(string tag, string id = "")
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.DetailedInfo(id));
+    }
+}
+
+/// <summary>
+///     Command module for faction hangar admin commands with alias.
+/// </summary>
+[Category("fhm")]
+public class FactionHangarAdminCommandsAlias : CommandModule
+{
+    /// <summary>
+    ///     Adds or removes a player from the whitelist.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="targetNameOrSteamId">The target player's name or Steam ID.</param>
+    [Command("whitelist", "Add or remove a player from whitelist")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Whitelist(string tag, string targetNameOrSteamId)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.ChangeWhitelist(targetNameOrSteamId));
+    }
+
+    /// <summary>
+    ///     Outputs the whitelisted Steam IDs.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    [Command("whitelisted", "Output the whitelisted steam ids")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Whitelist(string tag)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.ListWhitelist());
+    }
+
+    /// <summary>
+    ///     Changes the webhook.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="webhook">The new webhook URL.</param>
+    [Command("webhook", "Change the webhook")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Webhook(string tag, string webhook)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.ChangeWebhook(webhook));
+    }
+
+    /// <summary>
+    ///     Saves the grid the player is looking at to the hangar.
+    /// </summary>
+    /// <param name="gridName">The name of the grid.</param>
+    [Command("save", "Saves the grid you are looking at to hangar")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void SaveGrid(string gridName)
+    {
+        var user = new FactionAdminChecks(Context);
+        await HangarCommandSystem.RunAdminTaskAsync(void () => user.SaveGrid(gridName));
+    }
+
+    /// <summary>
+    ///     Lists all the grids saved in the hangar.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    [Command("list", "Lists all the grids saved in your hangar")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void ListGrids(string tag)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.ListGrids());
+    }
+
+    /// <summary>
+    ///     Loads the specified grid by index number.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="id">The grid ID.</param>
+    /// <param name="loadNearPlayer">Whether to load the grid near the player.</param>
+    [Command("load", "Loads the specified grid by index number")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Load(string tag, string id, bool loadNearPlayer = false)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.LoadGrid(id, loadNearPlayer));
+    }
+
+    /// <summary>
+    ///     Removes the grid from the hangar.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="id">The grid ID.</param>
+    [Command("remove", "removes the grid from your hangar")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Remove(string tag, string id)
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.RemoveGrid(id));
+    }
+
+    /// <summary>
+    ///     Provides information about the current grid in the hangar.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="id">The grid ID (optional).</param>
+    [Command("info", "Provides some info of the current grid in your hangar")]
+    [Permission(MyPromoteLevel.Admin)]
+    public async void Info(string tag, string id = "")
+    {
+        var user = new FactionAdminChecks(tag, Context);
+        await HangarCommandSystem.RunAdminTaskAsync(() => user.DetailedInfo(id));
+    }
+}
---
+++ Quantumhangar/HangarChecks/Faction/FactionAdminChecks.cs
@@ -0,0 +1,371 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using QuantumHangar.Utils;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Character;
+using Sandbox.Game.World;
+using Torch.Commands;
+using VRage.Game;
+using VRage.Game.ModAPI;
+using VRageMath;
+using static QuantumHangar.Utils.CharacterUtilities;
+
+namespace QuantumHangar.HangarChecks;
+
+/// <summary>
+///     Class responsible for performing various administrative checks and operations for factions.
+/// </summary>
+public class FactionAdminChecks
+{
+    static readonly Logger Log = LogManager.GetCurrentClassLogger();
+    Chat _chat;
+    CommandContext _ctx;
+    MyFaction _faction;
+    string _tag;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="FactionAdminChecks" /> class with a faction tag.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <param name="ctx">The command context.</param>
+    public FactionAdminChecks(string tag, CommandContext ctx)
+    {
+        Initialize(ctx);
+        _tag = tag;
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="FactionAdminChecks" /> class without a faction tag.
+    /// </summary>
+    /// <param name="ctx">The command context.</param>
+    public FactionAdminChecks(CommandContext ctx)
+    {
+        Initialize(ctx);
+    }
+
+    FactionHanger FactionsHanger { get; set; }
+    static Settings Config => Hangar.Config;
+
+    /// <summary>
+    ///     Initializes the command context and chat.
+    /// </summary>
+    /// <param name="ctx">The command context.</param>
+    void Initialize(CommandContext ctx)
+    {
+        var inConsole = TryGetAdminPosition(ctx.Player);
+        _chat = new Chat(ctx, inConsole);
+        _ctx = ctx;
+    }
+
+    /// <summary>
+    ///     Checks if the admin position is valid.
+    /// </summary>
+    /// <param name="admin">The admin player.</param>
+    /// <returns>True if the admin position is invalid, otherwise false.</returns>
+    bool TryGetAdminPosition(IMyPlayer admin)
+    {
+        return admin?.GetPosition() == null;
+    }
+
+    /// <summary>
+    ///     Initializes the hangar for the specified faction tag.
+    /// </summary>
+    /// <param name="tag">The faction tag.</param>
+    /// <returns>True if the hangar was successfully initialized, otherwise false.</returns>
+    bool InitHangar(string tag)
+    {
+        _faction = MySession.Static.Factions.TryGetFactionByTag(tag);
+        if (_faction == null) return false;
+        var id = _faction.Members.First().Key;
+        var sid = MySession.Static.Players.TryGetSteamId(id);
+        // Prevents exception when faction is led by NPC.
+        if (sid == 0) return false;
+
+        FactionsHanger = new FactionHanger(sid, new Chat(_ctx));
+        return true;
+    }
+
+    /// <summary>
+    ///     Performs the main checks required before executing any faction operations.
+    /// </summary>
+    /// <returns>True if all checks pass, otherwise false.</returns>
+    bool PerformMainChecks()
+    {
+        if (!Config.PluginEnabled)
+        {
+            _chat?.Respond("Plugin is not enabled!");
+            return false;
+        }
+
+        if (_faction == null)
+        {
+            _chat?.Respond("Faction not found!");
+            return false;
+        }
+
+        if (FactionHanger.IsServerSaving(_chat))
+        {
+            _chat?.Respond("Server is saving or is paused!");
+            return false;
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    ///     Changes the webhook URL for the faction.
+    /// </summary>
+    /// <param name="webhook">The new webhook URL.</param>
+    public void ChangeWebhook(string webhook)
+    {
+        if (!InitHangar(_tag))
+        {
+            _chat?.Respond("Invalid Faction! Make sure the faction exists, and that it is not led by a NPC.");
+            return;
+        }
+
+        FactionsHanger.ChangeWebhook(webhook);
+        _chat?.Respond("Webhook changed.");
+    }
+
+    /// <summary>
+    ///     Changes the whitelist status of a player.
+    /// </summary>
+    /// <param name="targetNameOrSteamId">The target player's name or Steam ID.</param>
+    public void ChangeWhitelist(string targetNameOrSteamId)
+    {
+        if (!InitHangar(_tag))
+        {
+            _chat?.Respond("Invalid Faction! Make sure the faction exists, and that it is not led by a NPC.");
+            return;
+        }
+
+        if (TryGetPlayerSteamId(targetNameOrSteamId, _chat, out var steamId))
+        {
+            var result = FactionsHanger.ChangeWhitelist(steamId);
+            _chat?.Respond(result ? "Player added to whitelist." : "Player removed from whitelist.");
+        }
+        else
+        {
+            _chat?.Respond("Couldn't find that player!");
+        }
+    }
+
+    /// <summary>
+    ///     Saves the specified grid.
+    /// </summary>
+    /// <param name="name">The name of the grid.</param>
+    public async Task SaveGrid(string name)
+    {
+        var result = new GridResult(true);
+        if (!result.GetGrids(_chat, (MyCharacter)_ctx.Player?.Character, name)) return;
+
+        if (result.OwnerSteamId == 0)
+        {
+            _chat?.Respond("Unable to get major grid owner!");
+            return;
+        }
+
+        _faction = MySession.Static.Factions.GetPlayerFaction(result.BiggestOwner);
+        FactionsHanger = new FactionHanger(result.OwnerSteamId, _chat, true);
+
+        if (!PerformMainChecks()) return;
+
+        var gridData = result.GenerateGridStamp();
+        FactionsHanger.SelectedFactionFile.FormatGridName(gridData);
+
+        var val = await FactionsHanger.SaveGridsToFile(result, gridData.GridName, _faction.Members.First().Key);
+        if (val)
+        {
+            FactionsHanger.SaveGridStamp(gridData);
+            _chat?.Respond("Save Complete!");
+            FactionsHanger.SendWebHookMessage($"Admin saved grid {gridData.GridName}");
+        }
+        else
+        {
+            _chat?.Respond("Saved Failed!");
+        }
+    }
+
+    /// <summary>
+    ///     Lists all whitelisted players for the faction.
+    /// </summary>
+    public void ListWhitelist()
+    {
+        if (!InitHangar(_tag))
+        {
+            _chat?.Respond("Invalid Faction! Make sure the faction exists, and that it is not led by a NPC.");
+            return;
+        }
+
+        FactionsHanger.ListAllWhitelisted();
+    }
+
+    /// <summary>
+    ///     Lists all grids for the faction.
+    /// </summary>
+    public void ListGrids()
+    {
+        if (!InitHangar(_tag))
+        {
+            _chat?.Respond("Invalid Faction! Make sure the faction exists, and that it is not led by a NPC.");
+            return;
+        }
+
+        FactionsHanger.ListAllGrids();
+    }
+
+    /// <summary>
+    ///     Provides detailed information about a specific grid.
+    /// </summary>
+    /// <param name="input">The grid identifier.</param>
+    public void DetailedInfo(string input)
+    {
+        if (!InitHangar(_tag))
+        {
+            _chat?.Respond("Invalid Faction! Make sure the faction exists, and that it is not led by a NPC.");
+            return;
+        }
+
+        if (!FactionsHanger.ParseInput(input, out var id))
+        {
+            _chat.Respond($"Grid {input} could not be found!");
+            return;
+        }
+
+        FactionsHanger.DetailedReport(id);
+    }
+
+    /// <summary>
+    ///     Loads a grid for the faction.
+    /// </summary>
+    /// <param name="input">The grid identifier.</param>
+    /// <param name="loadNearPlayer">Whether to load the grid near the player.</param>
+    public void LoadGrid(string input, bool loadNearPlayer)
+    {
+        if (!InitHangar(_tag))
+        {
+            _chat?.Respond("Invalid Faction! Make sure the faction exists, and that it is not led by a NPC.");
+            return;
+        }
+
+        if (!PerformMainChecks()) return;
+
+        if (!FactionsHanger.ParseInput(input, out var id))
+        {
+            _chat.Respond($"Grid {input} could not be found!");
+            return;
+        }
+
+        if (!FactionsHanger.TryGetGridStamp(id, out var stamp)) return;
+
+        if (!FactionsHanger.LoadGrid(stamp, out var grids, _faction.Members.First().Key))
+        {
+            Log.Error($"Loading grid {id} failed for Admin!");
+            _chat.Respond("Loading grid failed! Report this to staff and check logs for more info!");
+            return;
+        }
+
+        var myObjectBuilderCubeGrids = grids as MyObjectBuilder_CubeGrid[] ?? grids.ToArray();
+        PluginDependencies.BackupGrid(myObjectBuilderCubeGrids.ToList(), _faction.Members.First().Key);
+
+        var pos = _ctx.Player?.Character?.GetPosition();
+        var spawnPos = DetermineSpawnPosition(stamp.GridSavePosition, pos.GetValueOrDefault(),
+            out var keepOriginalPosition, loadNearPlayer);
+
+        if (PluginDependencies.NexusInstalled && Config.NexusApi && NexusSupport.RelayLoadIfNecessary(spawnPos, id,
+                loadNearPlayer, _chat, _ctx.Player!.SteamUserId, _faction.Members.First().Key,
+                _ctx.Player!.GetPosition())) return;
+
+        var sid = MySession.Static.Players.TryGetSteamId(_faction.Members.First().Key);
+        // Prevents exception when faction is led by NPC.
+        if (sid == 0) return;
+
+        var spawner = new ParallelSpawner(myObjectBuilderCubeGrids, _chat, sid, SpawnedGridsSuccessful);
+        spawner.setBounds(stamp.BoundingBox, stamp.Box, stamp.MatrixTranslation);
+
+        Log.Info("Attempting Grid Spawning @" + spawnPos);
+        if (!spawner.Start(spawnPos, keepOriginalPosition)) return;
+        _chat?.Respond("Spawning Complete!");
+        FactionsHanger.RemoveGridStamp(id);
+        FactionsHanger.SendWebHookMessage("Admin loaded grid {stamp.GridName}");
+    }
+
+    /// <summary>
+    ///     Removes a grid from the faction.
+    /// </summary>
+    /// <param name="input">The grid identifier.</param>
+    public void RemoveGrid(string input)
+    {
+        if (!InitHangar(_tag))
+        {
+            _chat?.Respond("Invalid Faction! Make sure the faction exists, and that it is not led by a NPC.");
+            return;
+        }
+
+        if (!FactionsHanger.ParseInput(input, out var id))
+        {
+            _chat.Respond($"Grid {input} could not be found!");
+            return;
+        }
+
+        if (FactionsHanger.RemoveGridStamp(id))
+            _chat.Respond("Successfully removed grid!");
+    }
+
+    /// <summary>
+    ///     Callback method for successful grid spawning.
+    /// </summary>
+    /// <param name="grids">The set of spawned grids.</param>
+    void SpawnedGridsSuccessful(HashSet<MyCubeGrid> grids)
+    {
+        grids.BiggestGrid(out var biggestGrid);
+
+        if (_ctx.Player == null) return;
+
+        if (biggestGrid != null && _ctx.Player?.IdentityId != 0)
+            new GpsSender().SendGps(biggestGrid.PositionComp.GetPosition(), biggestGrid.DisplayName,
+                _ctx.Player.IdentityId);
+    }
+
+    /// <summary>
+    ///     Determines the spawn position for a grid.
+    /// </summary>
+    /// <param name="gridPosition">The original grid position.</param>
+    /// <param name="characterPosition">The character's position.</param>
+    /// <param name="keepOriginalPosition">Whether to keep the original position.</param>
+    /// <param name="playersSpawnNearPlayer">Whether to spawn near the player.</param>
+    /// <returns>The determined spawn position.</returns>
+    static Vector3D DetermineSpawnPosition(Vector3D gridPosition, Vector3D characterPosition,
+        out bool keepOriginalPosition, bool playersSpawnNearPlayer = false)
+    {
+        switch (Config.LoadType)
+        {
+            case LoadType.ForceLoadNearOriginalPosition when gridPosition == Vector3D.Zero:
+                Log.Info("Grid position is empty!");
+                keepOriginalPosition = false;
+                return characterPosition;
+            case LoadType.ForceLoadNearOriginalPosition:
+                Log.Info("Loading from grid save position!");
+                keepOriginalPosition = true;
+                return gridPosition;
+            case LoadType.ForceLoadMearPlayer when characterPosition == Vector3D.Zero:
+                keepOriginalPosition = true;
+                return gridPosition;
+            case LoadType.ForceLoadMearPlayer:
+                keepOriginalPosition = false;
+                return characterPosition;
+        }
+
+        if (playersSpawnNearPlayer)
+        {
+            keepOriginalPosition = false;
+            return characterPosition;
+        }
+
+        keepOriginalPosition = true;
+        return gridPosition;
+    }
+}
